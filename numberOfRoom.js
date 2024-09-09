7aa8a1f52fb1a1a361cd9923f380c615e6d37db7
Natalia Lyamina <natalia.lyamina1234@gmail.com>
2024-09-09 15:51:33 +0300

    dod

---
+++ numberOfRoom.js
@@ -0,0 +1,39 @@
+const numberOfRoom = [
+    {
+        name: 'HotelNumberOne',
+        number: 1,
+        category: 'Стандарт',
+        places: 1,
+        status: 'Свободен'},
+
+    {
+        name: 'HotelNumberTwo',
+        number: 2,
+        category: 'Люкс',
+        places: 2,
+        status: 'Свободен'},
+
+    {
+        name: 'HotelNumberThree',
+        number: 3,
+        category: 'Апартаменты',
+        places: 4,
+        status: 'Свободен'},
+        
+    {
+
+        name: 'HotelNumberFour',
+        number: 4,
+        category: 'Апартаменты',
+        places: 3,
+        status: 'Свободен'}, 
+        
+     {
+        name: 'HotelNumberFive',
+        number: 5,
+        category: 'Стандарт',
+        places: 2,
+        status: 'Свободен'},   
+];
+
+export default numberOfRoom;
